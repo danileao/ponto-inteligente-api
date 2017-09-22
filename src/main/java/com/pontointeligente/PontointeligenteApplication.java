976a4818082a8433211e43584ab94d69e643f179
dlevangelista <daniele.evangelista@unicid.edu.br>
2017-09-22 09:09:26 -0300

    Commit Inicial dos arquivos do projeto

---
+++ src/main/java/com/pontointeligente/PontointeligenteApplication.java
@@ -0,0 +1,12 @@
+package com.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontointeligenteApplication.class, args);
+	}
+}
